61242872f0174889892bda8870c17bf3eac22157
koustav-dhar <koustavdhar.31.05@gmail.com>
2021-10-21 02:12:41 +0530

    added data types, var declaration and input tag

---
+++ koustav-dhar/output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+
+printf("Hello, World\n");
+printf("- From Koustav\n");
+int num1=10,num2;
+char ch1='x',ch2;
+float fl1=1.23,fl2;
+printf("num1 = %d ch1 = %c fl1 = %f\n",num1,ch1,fl1);
+scanf("%c%d%f",&ch2,&num2,&fl2);
+printf("num2 = %d ch2 = %c fl2 = %f\n",num2,ch2,fl2);
+}
--- koustav-dhar/transpiler1.cpp
+++ koustav-dhar/transpiler1.cpp
@@ -3,12 +3,14 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <sstream>
 
 // class for the transpiler
 class Transpiler {
 private:
     std::unordered_map<std::string, std::string> dataMapper;    // symbol table to store all the keyword mappings
     std::vector<std::string> setParserData;                     // vector to store the transpiled keywords after parsing
+    std::unordered_map<std::string, std::string> variableMapper;
 public:
     Transpiler() {  // store the keyword mappings
         dataMapper.insert({"htpl", "#include <stdio.h>\n"});
@@ -16,30 +18,100 @@ public:
         dataMapper.insert({"log", "printf()"});
         dataMapper.insert({"/", "}"});
         dataMapper.insert({"/log", ";"});
+        dataMapper.insert({"in", "int"});
+        dataMapper.insert({"ch", "char"});
+        dataMapper.insert({"fl", "float"});
+        dataMapper.insert({"int", "%d"});
+        dataMapper.insert({"char", "%c"});
+        dataMapper.insert({"float", "%f"});
+        dataMapper.insert({"take", "scanf()"});
     }
-    // function to remove the front spaces from a line
-    std::string removeFrontSpaces(std::string getData) {  
-        for (int i = 0; i < getData.length(); ++i) {
-            if (getData[i] != ' ') {
-                getData = getData.substr(i, getData.length() - i);
-                break;
-            }
+    // function to remove the spaces from a line
+    std::string removeSpaces(std::string getData) {  
+        long long int ptr1 = 0, ptr2 = getData.length() - 1;
+        while (getData[ptr1] == ' ' || getData[ptr2] == ' ') {
+            if (getData[ptr1] == ' ' )
+                ++ptr1;
+            if (getData[ptr2] == ' ')
+                --ptr2;
         }
-        return getData;
+        return getData.substr(ptr1, ptr2 - ptr1 + 1);
     }
     // function to get keyword by removing the angular braces
     std::string stripBraces(std::string getData) {
         return getData.substr(1, getData.find('>') - 1);
     }
+    // function to add spaces between separators for i/o, var declaration tags
+    std::string spacify(std::string getData) {
+        std::string newData = "";
+        for (int i = 0; i < getData.length(); ++i) {
+            if (getData[i] == '=' || getData[i] == ',') {
+                newData += ' ';
+                newData += getData[i];
+                newData += ' ';
+            } else {
+                newData += getData[i];
+            }
+        }
+        return newData;
+    }
+    // parses an input tag or initialization tag(updates the variable map), and returns the tokens
+    std::vector<std::string> IOParser(std::string getData, bool io = false) {
+        getData = spacify(getData) + " ,";
+        std::vector<std::string> tokens;
+        std::stringstream dataStream(getData);
+        std::string word;
+        while (dataStream >> word) {    // tokenize using stringstream
+            tokens.push_back(word);
+        }
+        if (io == false) {
+            for (int i = 1; i < tokens.size(); ++i) {
+                // store the data type for variables
+                if (tokens[i] == "=" || (tokens[i] == "," && (i < 2 || tokens[i - 2] != "="))) {
+                    variableMapper.insert({tokens[i - 1], dataMapper.find(dataMapper.find(tokens[0])->second)->second});
+                }
+            }
+        }
+        return tokens;
+    }
     // function which parses a line
     // returns True if successfully parsed
     // returns False if error encountered from invalid tags
-    bool Parser(std::string getData, int lineNo, bool removeSpaces = true) {
-        if (removeSpaces == true)   // if new line
-            getData = removeFrontSpaces(getData);
+    bool Parser(std::string getData, int lineNo, bool removeSpace = true) {
+        if (removeSpace == true)   // if new line
+            getData = removeSpaces(getData);
         // std::cout << getData << "\n";
         if (getData[0] == '<') {    // if it's a tag
-            if (getData[1] != '/') {    // if opening tag
+            if (getData[getData.length() - 1] == '>' && getData[getData.length() - 2] == '/') { // if it's a self closing tag
+                getData = removeSpaces(getData.substr(1, getData.length() - 3));
+                if (dataMapper.find(getData.substr(0, 2)) != dataMapper.end()) {   // check if variable declaration or not
+                    setParserData.push_back(dataMapper.find(getData.substr(0, 2))->second + getData.substr(2) + ";");
+                    IOParser(getData);
+                } else if (getData.length() >= 4 && getData.substr(0, 4) == "take") {   // check for input tag
+                    std::vector<std::string> tokens = IOParser(getData, true);  // get the tokens
+                    std::string formatSpecifier = "", varNames = "";
+                    for (int i = 1; i < tokens.size(); ++i) {   // build the parameters for scanf
+                        if (tokens[i] == ",") {
+                            if (variableMapper.find(tokens[i - 1]) != variableMapper.end()) {
+                                formatSpecifier += variableMapper.find(tokens[i - 1])->second;
+                                if (varNames != "") {
+                                    varNames += ",";
+                                }
+                                varNames += ("&" + tokens[i - 1]);
+                            } else {    // undeclared variable input, error
+                                std::cout << "Error at Line " << lineNo << ": Input for undeclared variable \'" << tokens[i] << "\'.\n";
+                                return false;
+                            }
+                        }
+                    }
+                    setParserData.push_back(dataMapper.find(tokens[0])->second);
+                    setParserData.push_back("\"" + formatSpecifier + "\"," + varNames);
+                } else {    // invalid self closing tag
+                    std::cout << "Error at Line " << lineNo << ": Couldn\'t recognize tag \'" << getData << "\'.\n";
+                    return false;
+                }
+
+            } else if (getData[1] != '/') {    // if opening tag
                 std::string getTag = stripBraces(getData);
                 if (dataMapper.find(getTag) != dataMapper.end()) {
                     setParserData.push_back(dataMapper.find(getTag)->second);
@@ -59,9 +131,9 @@ public:
             }
             // pass the rest of the line(if any) after this tag for parsing
             int index = getData.find('>');
-            if (index + 1 < getData.length()) {
+            if (index != std::string::npos && index + 1 < getData.length()) {
                 getData = getData.substr(index + 1);
-                Parser(getData, lineNo, false);
+                Parser(getData, lineNo, true);
             }
         } else {    // if not a tag
             int index = getData.find('<');  // to print only text block, preceding any closing tag
@@ -71,7 +143,7 @@ public:
                 setParserData.back() += (" " + getData.substr(0, index));    
             }
             // pass the rest of the line(if any) after this text block for parsing
-            if (index < getData.length()) {
+            if (index != std::string::npos && index < getData.length()) {
                 getData = getData.substr(index);
                 Parser(getData, lineNo);
             }
@@ -84,10 +156,34 @@ public:
         for (int i = 0; i < setParserData.size(); ++i) {
             if (setParserData[i] == "printf()") {   // whenever print statement is encountered
                 // enclosing the text block within the printf function block and adding ';' at the end
-                string_const = setParserData[i].substr(0, 7) + '"' + setParserData[i + 1] + '"' + ")" + setParserData[i + 2];
+                std::string formatSpecifiers = "", varNames = "", varTemp = "";
+                for (int j = 0; j < setParserData[i + 1].length(); ++j) {
+                    if (j + 1 < setParserData[i + 1].length() && setParserData[i + 1].substr(j, 2) == "${") {
+                        int pos = setParserData[i + 1].find('}', j);
+                        varTemp = setParserData[i + 1].substr(j + 2, pos - (j + 2));
+                        if (variableMapper.find(varTemp) != variableMapper.end()) {
+                            formatSpecifiers += variableMapper.find(varTemp)->second;
+                            if (varNames.length() > 0) {
+                                varNames += ",";
+                            }
+                            varNames += varTemp;
+                        } else {
+                            // printing as it is, not error
+                            formatSpecifiers += setParserData[i + 1].substr(j, pos - j + 1);
+                        }
+                        j = pos;
+                    } else {
+                        formatSpecifiers += setParserData[i + 1][j];
+                    }
+                }
+                string_const = setParserData[i].substr(0, 7) + '"' + formatSpecifiers + '"' + (varNames != "" ? "," : "") + varNames + ")" + setParserData[i + 2];
                 setParserData[i] = string_const;
                 // erasing the text block and the ';' from the vector
                 setParserData.erase(std::next(setParserData.begin(), i + 1), std::next(setParserData.begin(), i + 3));
+            } else if (setParserData[i] == "scanf()") {
+                string_const = setParserData[i].substr(0, 6) + setParserData[i + 1] + ");";
+                setParserData[i] = string_const;
+                setParserData.erase(setParserData.begin() + i + 1);
             }
         }
     }
