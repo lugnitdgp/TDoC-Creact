d29be00c02f74094be9771ca1d7af974f2bc3861
Manika Biswas <71625968+b0nn1@users.noreply.github.com>
2021-10-25 11:09:23 +0530

    added multiple user input

---
+++ Manika/output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+
+int var1=500;
+char hf='p',k1='r';
+float a=500.8475;
+printf("The values are: %d, %d, %d, %f ",var1,hf,k1,a);
+
+int m=21,b;
+char c='*',d='&';
+float e;
+printf("Enter an integer and a float value respectively ");
+
+scanf("%d %f",&b,&e);
+printf("The values are %f, %d, %d, %d, %f \n",a,b,c,d,e);
+
+printf("tuyhi ti");
+
+printf("iucggc");
+
+}
