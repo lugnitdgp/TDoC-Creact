2907a4988d201ee735306360f99e4084420bae83
Manika Biswas <71625968+b0nn1@users.noreply.github.com>
2021-10-25 17:27:28 +0530

    added Loops

---
+++ Manika/output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+
+float a=500;
+int b=50;
+a=a+2;
+b=(b%2)+b;
+int i=0;
+printf("The value is %f \n",a);
+
+printf("The value is %d \n",b);
+
+for(i=0;i<b;i++){
+printf("%d\n",i);
+
+}
+printf("tuyhi ti");
+
+printf("iucggc");
+
+}
